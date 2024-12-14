578e411c1973485ec5c7fb0703094a3092752a29
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-12-14 20:44:33 +0530

    removed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,13 @@ const app = express();
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
-  })
+  cors()
+  //   {
+  //   origin: [
+  //     process.env.FRONTEND_URL || 'https://nexus-kitsunekode.vercel.app/',
+  //     'http://localhost:5173',
+  //   ],
+  // }
 );
 
 app.use('/api/auth', authRoutes);
